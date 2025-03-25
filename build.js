26577cb6f35b5a5aeb3fc4e8a4cba85c9561b74b
Noel Lago <nlagoglez@gmail.com>
2025-03-25 19:31:57 +0000

    update

--- build.js
+++ build.js
@@ -1,5 +1,37 @@
-async function run() {
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from 'sd-themes-loader';
+import { platform } from 'os';
 
+register(StyleDictionary, {
+    withSDBuiltins: false,
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
+async function run() {
+    const themes = await loader.load('/tokens');
+    const globalTheme = themes.getThemeByName('global');
+
+    const config = {
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/global/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+                transforms: [
+                    'name/camel',
+                    'ts/resolveMath',
+                    'size/pxToRem'
+                ]
+            }
+        }
+    }
+
+    globalTheme.addConfig(config).build();
 }
 
 run();
