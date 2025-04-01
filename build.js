aae2d42c0db909b6110e6f68cea89640463f4e40
Noel Lago <nlagoglez@gmail.com>
2025-04-01 08:35:39 +0000

    last update

--- build.js
+++ build.js
@@ -12,6 +12,7 @@ async function run() {
     const themes = await loader.load('/tokens');
     const globalTheme = themes.getThemeByName('global');
     const lightTheme = themes.getThemeByName('light');
+    const darkTheme = themes.getThemeByName('dark');
 
     const globalConfig = {
         expand:{
@@ -55,9 +56,30 @@ async function run() {
             }
         }
     };
+
+    const darkConfig= {
+        expand:{
+            typesMap: true,
+        },
+        platforms: {
+            web: {
+                files: [
+                    {
+                    destination: 'app/build/dark/variables.css',
+                    format: 'css/variables',
+                    }
+                ],
+                transforms: [
+                    'name/kebab',
+                    'color/rgb',
+                ]
+            }
+        }
+    };
     
     globalTheme.addConfig(globalConfig).build();
     lightTheme.addConfig(lightConfig).build();
+    darkTheme.addConfig(darkConfig).build();
 }
 
 run();
